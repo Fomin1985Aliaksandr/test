6c971bdd19d8eb10d2d9497c79975b712c4eeed6
aliaksandr <aliaksandr_fomin@list.ru>
2024-06-29 16:59:08 +0300

    newcomit2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
         public static void main (String[] arg) {
 
+
                  meters electro = new meters();
                  meters gas = new meters();
                  meters water = new meters();
@@ -14,26 +15,32 @@ public class Main {
                 gas.totalAmoutGas = gas.calc;
 
                  water.name = " water";
-                 water.readings = 110;
+                 water.readings = 122222222;
                  water.tariff = 5;
                  water.calc = water.readings* water.tariff;
                  water.totalAmoutWater = water.calc;
 
                 electro.name = "electro";
-                electro.readings= 220;
+                electro.readings= 2;
                 electro.tariff= 3;
                 electro.calc= electro.readings* electro.tariff;
                 electro.totalAmoutElectro = electro.calc;
 
-                 gas.calculation(gas.readings, gas.tariff);
+                 gas.calculation(gas.readings, gas.tariff) ;
                  water.calculation(water.readings, water.tariff);
                  sum.totalAmount(gas.calc, water.calc,electro.calc);
 
-                 System.out.println("Summa k oplate za gaz v tekuchem mesatse = "+ gas.calc);
-                 System.out.println("Summa k oplate za vodu v tekuchem mesatse = " + water.calc);
-                 System.out.println("Summa k oplate za electro v tekuchem mesatse = " + electro.calc);
-                 System.out.println("Vsego k oplate " + sum.totalAmount(water.calc,+gas.calc, electro.calc));
 
+
+
+
+                 System.out.println("Summa k oplate za gaz v tekuchem mesatse = "+ gas.calc + "$");
+                 System.out.println("Summa k oplate za vodu v tekuchem mesatse = " + water.calc + "$");
+                 System.out.println("Summa k oplate za electro v tekuchem mesatse = " + electro.calc + "$");
+                 System.out.println("Vsego k oplate bez skidki " + sum.totalAmount(water.calc,+gas.calc, electro.calc)+"$");
+
+            int a = sum.totalAmount(gas.calc, water.calc,electro.calc);
+            if (a<500) {}else {System.out.println("Oplata so skidrkoy  " + a/ sum.skidka+"$"+".Razmer skidki=50%" );}
 
 
 
--- src/main/java/meters.java
+++ src/main/java/meters.java
@@ -6,12 +6,15 @@ public class meters {
     int totalAmoutGas;
     int totalAmoutWater;
     int totalAmoutElectro;
+    int skidka=2;
 
 
 
 
     public int calculation (int readings,int tariff){return readings*tariff;}
         public int totalAmount (int totalAmoutGas,int totalAmoutWater,int totalAmoutElectro) {return totalAmoutWater+totalAmoutGas+totalAmoutElectro;}
+
+
 
 
 }
