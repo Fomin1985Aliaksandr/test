e40ef678468ababe1e4c5e515018c4b5f7d15093
aliaksandr <aliaksandr_fomin@list.ru>
2024-06-29 13:51:46 +0300

    newcomit2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,53 @@
-import java.util.Scanner;
 public class Main {
-    public static void main(String[] arg) {
+
+        public static void main (String[] arg) {
+
+                 meters electro = new meters();
+                 meters gas = new meters();
+                 meters water = new meters();
+                 meters sum   =  new meters();
+
+                gas.name = "gas";
+                gas.readings = 10;
+                gas.tariff = 2;
+                gas.calc= gas.readings* gas.tariff;
+                gas.totalAmoutGas = gas.calc;
+
+                 water.name = " water";
+                 water.readings = 110;
+                 water.tariff = 5;
+                 water.calc = water.readings* water.tariff;
+                 water.totalAmoutWater = water.calc;
+
+                electro.name = "electro";
+                electro.readings= 220;
+                electro.tariff= 3;
+                electro.calc= electro.readings* electro.tariff;
+                electro.totalAmoutElectro = electro.calc;
+
+                 gas.calculation(gas.readings, gas.tariff);
+                 water.calculation(water.readings, water.tariff);
+                 sum.totalAmount(gas.calc, water.calc,electro.calc);
+
+                 System.out.println("Summa k oplate za gaz v tekuchem mesatse = "+ gas.calc);
+                 System.out.println("Summa k oplate za vodu v tekuchem mesatse = " + water.calc);
+                 System.out.println("Summa k oplate za electro v tekuchem mesatse = " + electro.calc);
+                 System.out.println("Vsego k oplate " + sum.totalAmount(water.calc,+gas.calc, electro.calc));
 
 
 
 
-
-
-
-
-            }
         }
+}
 
 
-
-
-//
-//    String scan;
-//    Scanner scanner = new Scanner(System.in);
-//        while (true) {
-//                System.out.println("Otgadai zagadky. Zimoi i letom odnim tsvetom ");
-//                scan = scanner.nextLine();
-//                if (scan.equals("elka")) { System.out.println("Pravelni otvet.");
-//                break;
-//                } else {
-//                System.out.println("Nepravilni otvet.");
+////
+////    String scan;
+////    Scanner scanner = new Scanner(System.in);
+////        while (true) {
+////                System.out.println("Otgadai zagadky. Zimoi i letom odnim tsvetom ");
+////                scan = scanner.nextLine();
+////                if (scan.equals("elka")) { System.out.println("Pravelni otvet.");
+////                break;
+////                } else {
+////                System.out.println("Nepravilni otvet.");
--- src/main/java/meters.java
+++ src/main/java/meters.java
@@ -1,21 +1,21 @@
-
 public class meters {
-
     String name;
-
     int readings;
-
     int tariff;
+    int calc;
+    int totalAmoutGas;
+    int totalAmoutWater;
+    int totalAmoutElectro;
 
-    public static void main (String[] arg) {
-        meters gas = new meters();
 
-        gas.name = "gas";
-        gas.readings=10;
-        gas.tariff = 2;
 
-       System.out.println("Мы сздали счетчик по"+gas.name + " Показания при установке" + gas.readings + "тариф на газ = " + gas.tariff);
 
-    }
+    public int calculation (int readings,int tariff){return readings*tariff;}
+        public int totalAmount (int totalAmoutGas,int totalAmoutWater,int totalAmoutElectro) {return totalAmoutWater+totalAmoutGas+totalAmoutElectro;}
+
 
 }
+
+
+
+
